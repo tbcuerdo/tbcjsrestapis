bdb028da1d3f65a3b452d0448c7c8955ae6ba33c
root <traceycuerdo@gmail.com>
2022-08-02 05:36:52 +0000

    Added comments

--- bethanyspieapi/index.js
+++ bethanyspieapi/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const pieRepo = require('./repos/pieRepo')
 
+// Configure middleware to support JSON data parsing in the request object
 app.use(express.json())
 
+// Create a GET api to retrieve all the pies
 app.get('/', (req, res, next) => {
     pieRepo.get((data) => {
         res.status(200).json({
@@ -16,6 +18,7 @@ app.get('/', (req, res, next) => {
     (err) => { next(err) });
 });
 
+// create a POST api to save 1 pie
 app.post('/', (req, res, next) => {
     let newData = req.body;
     console.log('newData: ', newData)
--- bethanyspieapi/repos/pieRepo.js
+++ bethanyspieapi/repos/pieRepo.js
@@ -2,19 +2,25 @@ const { FILE } = require('dns');
 const fs = require('fs');
 const FILE_NAME = './assets/pies.json';
 
+// create a get function to read the pies from a json file
 const get = (resolve, reject) => {
     let rs = fs.createReadStream(FILE_NAME);
+
+    // once data is available invoke the callback with the data
     rs.on('data', (data) => {
         console.log('data retrieved.')
         let parsed = JSON.parse(data)
         resolve(parsed);
     });
+
+    // in case of an error on the read stream invoke the reject callback
     rs.on('error', (err) => {
         console.log('error: ' + err);
         reject(err);
     });
 };
 
+// create an insert function to add the new pie in the list of pies inside the json file
 const insert = (newData, resolve, reject) => {
     console.log('insert...')
 
